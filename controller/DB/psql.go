b191a56e79691a150b8ea45e6940ea785a1b9175
Roger_Deng <s0905635919@gmail.com>
2023-04-05 04:39:53 +0800

    insert entryList and updating head of redis

--- controller/DB/psql.go
+++ controller/DB/psql.go
@@ -1,30 +1,61 @@
 package db
+
 import (
-    "DBIH/controller/encryption"
-    "database/sql"
-    "log"
-    _ "github.com/lib/pq"
+	"DBIH/controller/encryption"
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
 )
 
 var pdb *sql.DB = func() *sql.DB {
-    var db *sql.DB
-    var err error
-    db, err = sql.Open("postgres", "user=postgres password=passwd dbname=postgres sslmode=disable")
-    if err != nil {
-        log.Fatal(err)
-    }
-    return db
+	var db *sql.DB
+	var err error
+	db, err = sql.Open("postgres", "user=postgres password=passwd dbname=postgres sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
 }()
 
-// insert recommendation list in postgreSQL
-func PInsertList(UID string, list []string) {
-    pKey := encryption.Encrypt(UID)
-    _, err := pdb.Exec("INSERT INTO entryList (listKey,AID) VALUES ($1,$2)", pKey, list)
-    if err != nil {
-        log.Fatal(err)
-    }
+func PInsertEntryList(UID string, list []string) {
+	// check if UID exists in recommendation table
+	var tmp, eKey string
+	err := pdb.QueryRow("SELECT listKey FROM recommendations WHERE listKey = $1", UID).Scan(&tmp)
+	if err != nil { //
+		eKey := encryption.Encrypt(UID)
+		_, err := pdb.Exec("INSERT INTO recommendations (listKey,entryKey) VALUES ($1,$2)", eKey, UID)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		// if UID exists , exchange eKey with UID
+		eKey = encryption.Encrypt(UID)
+		_, err := pdb.Exec("UPDATE recommendations SET listKey = $1 WHERE listKey = $2", eKey, UID)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// delete the old list
+		_, err = pdb.Exec("DELETE FROM entryList WHERE listKey = $1", UID)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// check if UID exists in redis
+		_, err = RedisClient.Get(UID).Result()
+		if err == nil {
+			// if UID exists in redis, update the key
+			err := RedisClient.Set(UID, eKey, 0).Err()
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+	// insert the new list
+	_, err = pdb.Exec("INSERT INTO entryList (listKey,AID) VALUES ($1,$2)", eKey, list)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
-
 
 /*
 CREATE TABLE recommendations (
--- controller/DB/redis.go
+++ controller/DB/redis.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"github.com/go-redis/redis"
+	_ "github.com/lib/pq"
 )
 
 var RedisClient *redis.Client
@@ -12,40 +13,4 @@ func init() {
 		Password: "",
 		DB:       0,
 	})
-}
-
-// insert string , string  into redis
-func RInsertUIDHead(UID string, key string) {
-	err := RedisClient.Set(UID, key, 0).Err()
-	if err != nil {
-		panic(err)
-	}
-}
-
-// RGetUIDHead returns the key of the UID
-func RGetUIDHead(UID string) string {
-	var tmp string
-	key, err := RedisClient.Get(UID).Result()
-	// if UID not found in redis, check if it exists in postgreSQL
-	if err != nil {
-		tmp = PQuerryUIDHead(UID)
-		// if key exists, insert into redis
-		if tmp != "" {
-			RInsertUIDHead(UID, tmp)
-		}
-		return tmp
-	} 
-	return key
-}
-
-// Update UIDHead in redis
-func RUpdateUIDHead(UID string, key string) {
-	// check if UID exists in redis , if exists, update the key
-	_, err := RedisClient.Get(UID).Result()
-	if err == nil {
-		err := RedisClient.Set(UID, key, 0).Err()
-		if err != nil {
-			panic(err)
-		}
-	}
 }
